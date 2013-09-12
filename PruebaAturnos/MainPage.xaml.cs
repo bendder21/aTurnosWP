ad3fded05e32ab4faa4b70b35ce30c3003bb3323
bendder21 <bendder@gmail.com>
2013-09-13 01:42:57 +0200

    Primer commit

---
+++ PruebaAturnos/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+
+namespace PruebaAturnos
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+         //   WebBrowserTask pruebaweb = new WebBrowserTask();
+         //   pruebaweb.Uri = new Uri("www.aturnos.com/indexm.php");
+         //   pruebaweb.Show();
+            
+            navegador.Navigate(new Uri("http://www.aturnos.com/indexm.php"));
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            navegador.Navigate(new Uri("http://www.marca.com"));
+            System.Diagnostics.Debug.WriteLine("llega aqui");
+           
+        }
+
+    
+    }
+}
